ccf617376807f6afbbf1be0e580a004aec6da533
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2021-09-13 19:49:19 +0300

    [Task 2] Review fixes (CI)

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -69,36 +69,28 @@ def test_get_min_dfa() -> None:
     state_0 = State(0)
     state_1 = State(1)
     state_2 = State(2)
-    state_3 = State(3)
 
     symbol_a = Symbol("a")
     symbol_l = Symbol("l")
     symbol_e = Symbol("e")
-    symbol_x = Symbol("x")
 
     expected_dfa.add_start_state(state_0)
 
     expected_dfa.add_final_state(state_0)
     expected_dfa.add_final_state(state_1)
     expected_dfa.add_final_state(state_2)
-    expected_dfa.add_final_state(state_3)
 
     expected_dfa.add_transition(state_0, symbol_a, state_0)
     expected_dfa.add_transition(state_0, symbol_l, state_1)
     expected_dfa.add_transition(state_0, symbol_e, state_2)
-    expected_dfa.add_transition(state_0, symbol_x, state_3)
 
     expected_dfa.add_transition(state_1, symbol_l, state_1)
     expected_dfa.add_transition(state_1, symbol_e, state_2)
-    expected_dfa.add_transition(state_1, symbol_x, state_3)
 
     expected_dfa.add_transition(state_2, symbol_e, state_2)
-    expected_dfa.add_transition(state_2, symbol_x, state_3)
 
-    expected_dfa.add_transition(state_3, symbol_x, state_3)
+    actual_dfa = regex_to_min_dfa("a* l* e*")
 
-    actual_dfa = regex_to_min_dfa("a* l* e* x*")
-
-    assert actual_dfa.is_equivalent_to(expected_dfa) and len(actual_dfa.states) == len(
+    assert expected_dfa.is_equivalent_to(actual_dfa) and len(actual_dfa.states) == len(
         expected_dfa.states
     )
