6ba27c841edc6546e3ab00a3d60c4b102c078607
bozhnyukAlex <55096905+bozhnyukAlex@users.noreply.github.com>
2021-09-14 12:13:14 +0300

    Task 2 done: regez -> minimal dfa
    * [Task 2] First version of task 2: function regex -> dfa and tests
    
    * [Task 2] Review fixes
    
    * [Task 2] Review fixes (CI)
    
    * [Task 2] Review fixes (CI)
    
    * [Task 2] Review fixes
    
    Co-authored-by: Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>

---
+++ project/regex.py
@@ -0,0 +1,30 @@
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+from pyformlang.regular_expression import Regex
+
+__all__ = ["regex_to_min_dfa"]
+
+
+def regex_to_min_dfa(regex_str: str) -> DeterministicFiniteAutomaton:
+    """
+    Gets a string representation of regular expression and builds equivalent Deterministic Finite Automaton
+
+    Parameters
+    ----------
+    regex_str: str
+        String representation of regular expression
+
+    Returns
+    -------
+    DeterministicFiniteAutomaton
+        Deterministic Finite Automaton, which is equivalent to given regular expression
+
+    Raises
+    ------
+    MisformedRegexError
+        If there is wrong form of string representation of regular expression
+    """
+    regex = Regex(regex_str)
+    enfa = regex.to_epsilon_nfa()
+    dfa = enfa.to_deterministic()
+    dfa_min = dfa.minimize()
+    return dfa_min
