488e227eca046ba0562bee51827aeee460f4b6bd
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2021-10-05 17:45:26 +0300

    [Task 4] Fix review

--- project/boolean_matrices.py
+++ project/boolean_matrices.py
@@ -3,6 +3,8 @@ from pyformlang.finite_automaton import State, NondeterministicFiniteAutomaton
 
 __all__ = ["BooleanMatrices"]
 
+from scipy.sparse import dok_matrix
+
 
 class BooleanMatrices:
     """Class representing boolean adjacency matrices
@@ -30,6 +32,13 @@ class BooleanMatrices:
         self.state_indexes = {}
 
     def to_automaton(self):
+        """
+        Transforms BooleanMatrices into NFA
+        Returns
+        -------
+        nfa: NondeterministicFiniteAutomaton
+            Representation of BooleanMatrices as NFA
+        """
         automaton = NondeterministicFiniteAutomaton()
         for label, bool_matrix in self.bool_matrices.items():
             for s_from, s_to in zip(*bool_matrix.nonzero()):
@@ -53,6 +62,15 @@ class BooleanMatrices:
         return self.final_states.copy()
 
     def transitive_closure(self):
+        """
+        Computes transitive closure of boolean matrices
+        Returns
+        -------
+        tc: dok_matrix
+            Transitive closure of boolean matrices
+        """
+        if not self.bool_matrices.values():
+            return dok_matrix((1, 1))
         tc = sum(bm for bm in self.bool_matrices.values())
         prev_nnz = tc.nnz
         new_nnz = 0
@@ -65,18 +83,38 @@ class BooleanMatrices:
 
     @classmethod
     def from_automaton(cls, automaton):
+        """
+        Transforms NFA into BooleanMatrices
+        Parameters
+        ----------
+        automaton: NondeterministicFiniteAutomaton
+            NFA to transform
+        Returns
+        -------
+        obj: BooleanMatrices
+            BooleanMatrices object from NFA
+        """
         bm = cls()
         bm.num_states = len(automaton.states)
         bm.start_states = automaton.start_states
         bm.final_states = automaton.final_states
         bm.state_indexes = {state: idx for idx, state in enumerate(automaton.states)}
-        bm.bool_matrices = bm.create_boolean_matrices(automaton)
+        bm.bool_matrices = bm._create_boolean_matrices(automaton)
         return bm
 
     def intersect(self, other):
         """
         Returns a new class object containing
         the Kronecker products for given matrices
+
+        Parameters
+        ----------
+        other: BooleanMatrices
+            Right-hand side boolean matrix
+        Returns
+        -------
+        intersection: BooleanMatrices
+            Intersection of two boolean matrices
         """
         bm_res = BooleanMatrices()
         bm_res.num_states = self.num_states * other.num_states
@@ -102,7 +140,19 @@ class BooleanMatrices:
 
         return bm_res
 
-    def create_boolean_matrices(self, automaton: NondeterministicFiniteAutomaton):
+    def _create_boolean_matrices(self, automaton: NondeterministicFiniteAutomaton):
+        """
+        Build dict of boolean matrix for every automata label-key from nfa
+
+        Parameters
+        ----------
+        automaton: NondeterministicFiniteAutomaton
+            NFA to transform to matrix
+        Returns
+        -------
+        boolean_matrices: dict
+            Dict of boolean matrix for every automata label-key
+        """
         bool_matrices = {}
         for s_from, trans in automaton.to_dict().items():
             for label, states_to in trans.items():
