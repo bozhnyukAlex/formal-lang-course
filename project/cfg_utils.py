64b82d14e61cd820cf2a480dcd1b94c51c36a989
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2021-10-20 23:02:27 +0300

    [Task 6] Review fixes

--- project/cfg_utils.py
+++ project/cfg_utils.py
@@ -1,8 +1,15 @@
 import os
 
-from pyformlang.cfg import CFG, Variable
+from pyformlang.cfg import CFG, Variable, Production, Epsilon
 
-__all__ = ["get_cnf_from_file", "get_cnf_from_text"]
+__all__ = [
+    "get_cnf_from_file",
+    "get_cnf_from_text",
+    "get_wcnf_from_file",
+    "get_wcnf_from_text",
+    "is_wcnf",
+    "get_original_csg_from_file",
+]
 
 
 def get_cnf_from_file(path: str, start_symbol: str = None) -> CFG:
@@ -37,17 +44,114 @@ def get_cnf_from_file(path: str, start_symbol: str = None) -> CFG:
         If file text is not satisfied to the rules
     """
 
+    __check_path(path)
+
+    with open(path, "r") as file:
+        cfg_str = file.read()
+
+    return get_cnf_from_text(cfg_str, start_symbol)
+
+
+def get_wcnf_from_file(path: str, start_symbol: str = None) -> CFG:
+    """
+    Gets Context Free Grammar in Weak Chomsky Normal Form
+    equivalent to file text representation of CFG.
+
+    Parameters
+    ----------
+    path: str
+        A path to file containing text representation of CFG with rules:
+        The structure of a production is: head -> body_1 | body_2 | … | body_n
+        A variable (or non terminal) begins by a capital letter
+        A terminal begins by a non-capital character
+        Terminals and Variables are separated by spaces
+        An epsilon symbol can be represented by epsilon, $, ε, ϵ or Є
+    start_symbol: str, default = None
+        An axiom for CFG
+        If not specified, 'S' will be used
+
+    Returns
+    -------
+    CNF:
+        Context Free Grammar in Chomsky Normal Form equivalent to
+        file text representation of CFG
+    Raises
+    ------
+    OSError:
+        If there was an error while working with file
+    ValueError:
+        If file text is not satisfied to the rules
+    """
+
+    __check_path(path)
+
+    with open(path, "r") as file:
+        cfg_str = file.read()
+
+    return get_wcnf_from_text(cfg_str, start_symbol)
+
+
+def get_original_csg_from_file(path: str, start_symbol: str = None) -> CFG:
+    """
+    Gets Context Free Grammar equivalent to file text representation of CFG.
+
+    Parameters
+    ----------
+    path: str
+        A path to file containing text representation of CFG with rules:
+        The structure of a production is: head -> body_1 | body_2 | … | body_n
+        A variable (or non terminal) begins by a capital letter
+        A terminal begins by a non-capital character
+        Terminals and Variables are separated by spaces
+        An epsilon symbol can be represented by epsilon, $, ε, ϵ or Є
+    start_symbol: str, default = None
+        An axiom for CFG
+        If not specified, 'S' will be used
+
+    Returns
+    -------
+    CNF:
+        Context Free Grammar in Chomsky Normal Form equivalent to
+        file text representation of CFG
+    Raises
+    ------
+    OSError:
+        If there was an error while working with file
+    ValueError:
+        If file text is not satisfied to the rules
+    """
+
+    if start_symbol is None:
+        start_symbol = "S"
+
+    __check_path(path)
+
+    with open(path, "r") as file:
+        cfg_str = file.read()
+    cfg = CFG.from_text(cfg_str, Variable(start_symbol))
+    return cfg
+
+
+def __check_path(path: str) -> None:
+    """
+    Checks whether path is representing a non-empty file with txt extension.
+
+    Parameters
+    ----------
+    path: str
+        A path to file containing text representation of CFG
+
+    Raises
+    ------
+    OSError:
+        If file does not exist or its extension is not txt or it is empty.
+    """
     if not os.path.exists(path):
         raise OSError("Wrong file path specified: file is not exists")
     if not path.endswith(".txt"):
         raise OSError("Wrong file path specified: *.txt is required")
     if os.path.getsize(path) == 0:
         raise OSError("Wrong file path specified: file is empty")
-
-    with open(path, "r") as file:
-        cfg_str = file.read()
-
-    return get_cnf_from_text(cfg_str, start_symbol)
 
 
 def get_cnf_from_text(cfg_text: str, start_symbol: str = None) -> CFG:
@@ -86,3 +190,92 @@ def get_cnf_from_text(cfg_text: str, start_symbol: str = None) -> CFG:
     cnf = cfg.to_normal_form()
 
     return cnf
+
+
+def get_wcnf_from_text(cfg_text: str, start_symbol: str = None) -> CFG:
+    """
+    Get context Free Grammar in Weak Chomsky Normal Form
+    equivalent to file text representation of CFG.
+
+    Parameters
+    ----------
+    cfg_text: str
+        Text representation of CFG with rules:
+        The structure of a production is: head -> body1 | body2 | … | bodyn
+        A variable (or non terminal) begins by a capital letter
+        A terminal begins by a non-capital character
+        Terminals and Variables are separated by spaces
+        An epsilon symbol can be represented by epsilon, $, ε, ϵ or Є
+    start_symbol: str, default = None
+        An axiom for CFG
+        If not specified, 'S' will be used
+    Returns
+    -------
+    CNF:
+        Context Free Grammar in Chomsky Normal Form equivalent to
+        file text representation of CFG
+    Raises
+    ------
+    ValueError:
+        If file text is not satisfied to the rules
+    """
+
+    if start_symbol is None:
+        start_symbol = "S"
+
+    cfg = CFG.from_text(cfg_text, Variable(start_symbol))
+
+    wcnf = (
+        cfg.remove_useless_symbols()
+        .eliminate_unit_productions()
+        .remove_useless_symbols()
+    )
+
+    new_productions = wcnf._get_productions_with_only_single_terminals()
+    new_productions = wcnf._decompose_productions(new_productions)
+
+    return CFG(start_symbol=wcnf.start_symbol, productions=set(new_productions))
+
+
+def __check_epsilons(reachable_symbols, productions_old, productions_nf):
+    """
+    Test whether all epsilons in reachable variables from initial grammar are present in given normal form
+    """
+    productions_old_with_epsilon = set(
+        filter(
+            lambda prod: prod.head in reachable_symbols and not prod.body,
+            productions_old,
+        )
+    )
+    productions_nf_with_epsilon = set(
+        filter(lambda prod: not prod.body, productions_nf)
+    )
+    for production in productions_old_with_epsilon:
+        if production not in productions_nf_with_epsilon:
+            return False
+    return True
+
+
+def is_wcnf(cfg_nf, cfg_old):
+    """
+    Test whether given cfg_nf is in Minimal Weakened Chomsky Normal Form
+    The rules are:
+    (1) A -> BC, where A, B, C in Variables
+    (2) A -> a, where A in Variables, a in Terminals
+    (3) A -> epsilon, where A in Variables
+    It is also checked whether every reachable epsilon production from original grammar is present in WNCF
+    """
+    for production in cfg_nf.productions:
+        body = production.body
+        if not (
+            # check (1)
+            (len(body) <= 2 and all(map(lambda x: x in cfg_nf.variables, body)))
+            # check (2)
+            or (len(body) == 1 and body[0] in cfg_nf.terminals)
+            # check (3)
+            or (not body)
+        ) or not __check_epsilons(
+            cfg_nf.variables, cfg_old.productions, cfg_nf.productions
+        ):
+            return False
+    return True
--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -1,6 +1,12 @@
 import pytest
+from pyformlang.cfg import Production, Variable, Terminal, Epsilon
 
-from project.cfg_utils import get_cnf_from_file
+from project.cfg_utils import (
+    get_cnf_from_file,
+    get_wcnf_from_file,
+    is_wcnf,
+    get_original_csg_from_file,
+)
 
 
 def test_wrong_file():
@@ -23,11 +29,132 @@ def test_wrong_text():
 
 @pytest.mark.parametrize(
     "filename, axiom",
-    [("epsilon_cfg.txt", "epsilon"), ("class_example.txt", "S"), ("random.txt", "NP")],
+    [
+        ("epsilon_cfg.txt", "E"),
+        ("example_1.txt", "S"),
+        ("random.txt", "NP"),
+        ("example_2.txt", "S"),
+        ("example_3.txt", "S"),
+        ("example_4.txt", "S"),
+        ("math.txt", "Expr"),
+    ],
 )
-def test_get_cnf_from_file(filename, axiom):
+def test_cnf_from_file_start_symbol(filename, axiom):
     path = "tests/data/cfgs/" + filename
 
+    wcnf = get_wcnf_from_file(path, axiom)
+
+    assert wcnf.start_symbol == Variable(axiom)
+
+
+@pytest.mark.parametrize(
+    "filename, axiom, productions",
+    [
+        ("epsilon_cfg.txt", "E", {Production(Variable("E"), [Epsilon()])}),
+        (
+            "example_1.txt",
+            "S",
+            {
+                Production(Variable("C#CNF#1"), [Variable("S"), Variable("b#CNF#")]),
+                Production(Variable("S"), [Variable("a#CNF#"), Variable("C#CNF#1")]),
+                Production(Variable("S"), [Variable("S"), Variable("S")]),
+                Production(Variable("b#CNF#"), [Terminal("b")]),
+                Production(Variable("S"), []),
+                Production(Variable("a#CNF#"), [Terminal("a")]),
+            },
+        ),
+        (
+            "random.txt",
+            "NP",
+            {
+                Production(Variable("CN"), [Terminal("boy")]),
+                Production(Variable("Det"), [Terminal("the")]),
+                Production(Variable("Wh"), [Terminal("whom")]),
+                Production(Variable("S/NP"), [Variable("NP"), Variable("VP/NP")]),
+                Production(Variable("VTrans"), [Terminal("hates")]),
+                Production(Variable("NP"), [Variable("Det"), Variable("CN")]),
+                Production(Variable("VP/NP"), [Variable("VTrans"), Variable("NP")]),
+                Production(Variable("NP"), [Terminal("mark")]),
+                Production(Variable("C#CNF#1"), [Variable("Wh"), Variable("S/NP")]),
+                Production(Variable("CN"), [Variable("CN"), Variable("C#CNF#1")]),
+            },
+        ),
+        (
+            "math.txt",
+            "Expr",
+            {
+                Production(Variable("MulOp"), [Terminal("div")]),
+                Production(Variable("Factor"), [Terminal("variable")]),
+                Production(Variable("Factor"), [Terminal("number")]),
+                Production(Variable("Term"), [Terminal("number")]),
+                Production(Variable("Term"), [Terminal("variable")]),
+                Production(Variable("Expr"), [Terminal("variable")]),
+                Production(Variable("Expr"), [Terminal("number")]),
+                Production(Variable("MulOp"), [Terminal("mul")]),
+                Production(Variable("Primary"), [Terminal("variable")]),
+                Production(Variable("AddOp"), [Terminal("sub")]),
+                Production(Variable("AddOp"), [Terminal("add")]),
+                Production(Variable("Primary"), [Terminal("number")]),
+                Production(Variable("Expr"), [Variable("Expr"), Variable("C#CNF#3")]),
+                Production(
+                    Variable("C#CNF#2"), [Variable("pow#CNF#"), Variable("Primary")]
+                ),
+                Production(
+                    Variable("Factor"), [Variable("Factor"), Variable("C#CNF#2")]
+                ),
+                Production(
+                    Variable("C#CNF#1"), [Variable("MulOp"), Variable("Factor")]
+                ),
+                Production(Variable("Term"), [Variable("Term"), Variable("C#CNF#1")]),
+                Production(Variable("Expr"), [Variable("AddOp"), Variable("Term")]),
+                Production(Variable("C#CNF#3"), [Variable("AddOp"), Variable("Term")]),
+                Production(Variable("pow#CNF#"), [Terminal("pow")]),
+                Production(Variable("Term"), [Variable("Factor"), Variable("C#CNF#2")]),
+                Production(Variable("Expr"), [Variable("Term"), Variable("C#CNF#1")]),
+                Production(Variable("Expr"), [Variable("Factor"), Variable("C#CNF#2")]),
+            },
+        ),
+        (
+            "example_2.txt",
+            "S",
+            {
+                Production(Variable("C#CNF#2"), [Variable("c#CNF#"), Variable("B")]),
+                Production(Variable("A"), [Variable("a#CNF#"), Variable("C#CNF#1")]),
+                Production(Variable("C#CNF#1"), [Variable("B"), Variable("C#CNF#2")]),
+                Production(Variable("B"), [Variable("d#CNF#"), Variable("C#CNF#3")]),
+                Production(
+                    Variable("C#CNF#3"), [Variable("e#CNF#"), Variable("f#CNF#")]
+                ),
+                Production(Variable("f#CNF#"), [Terminal("f")]),
+                Production(Variable("S"), [Variable("A"), Variable("B")]),
+                Production(Variable("c#CNF#"), [Terminal("c")]),
+                Production(Variable("d#CNF#"), [Terminal("d")]),
+                Production(Variable("a#CNF#"), [Terminal("a")]),
+                Production(Variable("e#CNF#"), [Terminal("e")]),
+            },
+        ),
+    ],
+)
+def test_cnf_from_file_productions(filename, axiom, productions):
+    path = "tests/data/cfgs/" + filename
+    wcnf = get_wcnf_from_file(path, axiom)
+    assert set(wcnf.productions) == set(productions)
+
+
+@pytest.mark.parametrize(
+    "filename, axiom",
+    [
+        ("epsilon_cfg.txt", "E"),
+        ("random.txt", "NP"),
+        ("example_2.txt", "S"),
+        ("example_4.txt", "S"),
+        ("math.txt", "Expr"),
+    ],
+)
+def test_get_wcnf_from_file(filename, axiom):
+    path = "tests/data/cfgs/" + filename
+
+    csg_old = get_original_csg_from_file(path, axiom)
     cnf = get_cnf_from_file(path, axiom)
 
-    assert cnf.is_normal_form()
+    assert is_wcnf(cnf, csg_old)
