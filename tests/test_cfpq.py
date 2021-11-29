4316d0e621f2b398569896bfa98a8922dc9536cf
bozhnyukAlex <55096905+bozhnyukAlex@users.noreply.github.com>
2021-11-29 18:20:31 +0300

    [Task 11] CFPQ tensor (#11)
    * [Task 11] CFPQ tensor

--- tests/test_cfpq.py
+++ tests/test_cfpq.py
@@ -5,76 +5,14 @@ import pytest
 from cfpq_data import labeled_cycle_graph
 from pyformlang.cfg import CFG
 
-from project import generate_two_cycles_graph, matrix_cfpq, hellings_cfpq
-from project import hellings
+from project import generate_two_cycles_graph, matrix_cfpq, hellings_cfpq, tensor_cfpq
 
 Config = namedtuple("Config", ["start_var", "start_nodes", "final_nodes", "exp_ans"])
 
 
-@pytest.fixture(params=[matrix_cfpq, hellings_cfpq])
+@pytest.fixture(params=[matrix_cfpq, hellings_cfpq, tensor_cfpq])
 def cfpq(request):
     return request.param
-
-
-@pytest.mark.parametrize(
-    "cfg, graph, exp_ans",
-    [
-        (
-            """
-            S -> epsilon
-            """,
-            labeled_cycle_graph(3, "a", verbose=False),
-            {(1, "S", 1), (2, "S", 2), (0, "S", 0)},
-        ),
-        (
-            """
-                S -> b | epsilon
-                """,
-            labeled_cycle_graph(4, "b", verbose=False),
-            {
-                (1, "S", 1),
-                (2, "S", 2),
-                (0, "S", 0),
-                (3, "S", 3),
-                (0, "S", 1),
-                (1, "S", 2),
-                (2, "S", 3),
-                (3, "S", 0),
-            },
-        ),
-        (
-            """
-                S -> A B
-                S -> A S1
-                S1 -> S B
-                A -> a
-                B -> b
-                """,
-            generate_two_cycles_graph(2, 1, ("a", "b")),
-            {
-                (0, "S1", 3),
-                (2, "S1", 0),
-                (2, "S", 3),
-                (2, "S1", 3),
-                (3, "B", 0),
-                (1, "S", 0),
-                (0, "S", 0),
-                (1, "S", 3),
-                (1, "A", 2),
-                (0, "S", 3),
-                (0, "B", 3),
-                (1, "S1", 3),
-                (2, "A", 0),
-                (1, "S1", 0),
-                (0, "S1", 0),
-                (0, "A", 1),
-                (2, "S", 0),
-            },
-        ),
-    ],
-)
-def test_hellings_answer(cfg, graph, exp_ans):
-    assert hellings(graph, CFG.from_text(cfg)) == exp_ans
 
 
 @pytest.mark.parametrize(
